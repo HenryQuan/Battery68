21f292011ed114743090c13bc70b2f5172c12df9
Yiheng <development.henryquan@gmail.com>
2018-10-14 20:47:38 +1100

    light status bar

--- battery/ViewController.swift
+++ battery/ViewController.swift
@@ -17,9 +17,20 @@ class ViewController: UIViewController {
         return Int(UIDevice.current.batteryLevel * 100)
     }
     
+    // Get colour according to current percentage
+    var background: UIColor {
+        // Material green
+        if (battery > 80) { return UIColorFromRGB(rgb: 0x8BC34A) }
+        // Material orange
+        if (battery > 20) { return UIColorFromRGB(rgb: 0xFF9800) }
+        // Material red
+        return UIColorFromRGB(rgb: 0xF44336)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.BatteryLabel.text = "\(battery)%"
+        self.view.backgroundColor = background
         
         // Add observer to update percentage with updated
         NotificationCenter.default.addObserver(self, selector: #selector(batteryLevelDidChange), name: UIDevice.batteryLevelDidChangeNotification, object: nil)
@@ -27,7 +38,19 @@ class ViewController: UIViewController {
     
     @objc func batteryLevelDidChange(_ notification: Notification) {
         self.BatteryLabel.text = "\(battery)%"
+        self.view.backgroundColor = background
     }
+    
+    // From: https://stackoverflow.com/questions/24074257/how-can-i-use-uicolorfromrgb-in-swift
+    func UIColorFromRGB(rgb: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgb & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgb & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgb & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+
     
 }
 
