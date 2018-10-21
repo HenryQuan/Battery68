fb29bc2cc7b31edf650264826b0a078d94faf3c3
Yiheng <development.henryquan@gmail.com>
2018-10-21 19:14:30 +1100

    add BatteryUtil and minitor battery change

--- Project/Android/app/src/main/java/org/github/henryquan/battery/MainActivity.kt
+++ Project/Android/app/src/main/java/org/github/henryquan/battery/MainActivity.kt
@@ -1,59 +1,38 @@
 package org.github.henryquan.battery
 
-import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.os.BatteryManager
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
-import android.widget.Toast
-import java.lang.Class.*
-import android.os.Build
-
-
-
+import org.github.henryquan.battery.Core.BatteryUtil
 
 class MainActivity : AppCompatActivity() {
+
+    val battery = BatteryUtil(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val curr = getCurrentCapacity(this)
-        val max = getDesignedCapacity()
-        val percentage = String.format("%.2f", (curr / max * 100))
-        capacityLabel.text = "${curr} / ${max} (${percentage}%)"
+        // get an estimated value
+        capacityLabel.text = battery.getEstimatedBatteryHealth()
+
+        // add boardcast receiver to track battery level
+        registerReceiver(this.broadCastReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED));
     }
 
     /**
-     * Get device designed capacity and convert to a string
-     * @return Unknown or Designed capacity
+     * To update when battery level changed
      */
-    fun getDesignedCapacity(): Double {
-        val POWER_PROFILE_CLASS = "com.android.internal.os.PowerProfile"
-        try {
-            val mPowerProfile_ = forName(POWER_PROFILE_CLASS).getConstructor(Context::class.java).newInstance(this)
-            val batteryCapacity = forName(POWER_PROFILE_CLASS)
-                .getMethod("getAveragePower", java.lang.String::class.java)
-                .invoke(mPowerProfile_, "battery.capacity") as Double
-            return batteryCapacity
-        } catch (e: Exception) {
-            e.printStackTrace()
+    val broadCastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(contxt: Context?, intent: Intent?) {
+            val level = intent?.getIntExtra("level", 0)
+            Log.d("Level", "${level}")
+            capacityLabel.text = battery.getEstimatedBatteryHealth()
         }
-        return 0.0
     }
-
-    /**
-     * Get current battery capacity
-     * @return current capacity
-     */
-    fun getCurrentCapacity(context: Context): Double {
-        val batteryManager = context.getSystemService(Context.BATTERY_SERVICE) as BatteryManager
-        val chargeCounter = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CHARGE_COUNTER).toDouble()
-        val percentage = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY).toDouble()
-        return chargeCounter / (percentage / 100) / 1000
-    }
-
 }
