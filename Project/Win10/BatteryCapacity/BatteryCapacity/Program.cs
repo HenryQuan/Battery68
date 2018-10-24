e209f069cfbf1dc105709ed3f12096f432184e82
Yiheng <HenryQuan@users.noreply.github.com>
2018-10-24 15:38:08 +1100

    grab info from html

--- Project/Win10/BatteryCapacity/BatteryCapacity/Program.cs
+++ Project/Win10/BatteryCapacity/BatteryCapacity/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace BatteryCapacity
 {
     class Program
     {
         const string BATTERY_REPORT = "battery-report.html";
+        // A template for regex to get data from html
+        const string REGEX_TEMPLATE = "{0}</span></td><td>(.*)";
 
         static void Main(string[] args)
         {
@@ -30,9 +33,56 @@ namespace BatteryCapacity
             File.Delete(BATTERY_REPORT);
         }
 
+        /// <summary>
+        /// Get designed capacity and current capacity
+        /// use simple division to get a estimate
+        /// </summary>
+        /// <returns></returns>
         static double CalcBatteryCapacity()
         {
+            // Read all text from html
+            var html = File.ReadAllText(BATTERY_REPORT);
+            // Get the information we want
+            var design = GetRegexMatch("DESIGN CAPACITY", html);
+            var curr = GetRegexMatch("FULL CHARGE CAPACITY", html);
+            var cycle = GetRegexMatch("CYCLE COUNT", html);
+
             return 99.9;
+        }
+
+        /// <summary>
+        /// Print the information and return the value
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="html"></param>
+        /// <returns></returns>
+        static string GetRegexMatch(string input, string html)
+        {
+            var regex = new Regex(String.Format(REGEX_TEMPLATE, input));
+            var match = regex.Match(html);
+            if (match.Length > 1)
+            {
+                // 1 is the macth
+                var value = match.Groups[1].ToString();
+                Console.WriteLine($"{input}: {value}");
+                return value;
+            }
+            else
+            {
+                return "Unkown";
+            }
+        }
+
+        /// <summary>
+        /// Convert 63,123 mah to 63123.0
+        /// </summary>
+        /// <param name="capacity"></param>
+        /// <returns></returns>
+        static double Normalise(string capacity)
+        {
+            var noMWh = capacity.Replace(" mWh", "");
+            var noComma = noMWh.Replace(",", "");
+            return Convert.ToDouble(noComma);
         }
 
         /// <summary>
