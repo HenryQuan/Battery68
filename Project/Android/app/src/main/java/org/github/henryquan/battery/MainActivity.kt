ff0a195254839e2b76610526d2eea3ed2927b0dd
Yiheng <development.henryquan@gmail.com>
2018-12-15 20:59:16 +0800

    I was on the wrong branch lol, fix logical issue

--- Project/Android/app/src/main/java/org/github/henryquan/battery/MainActivity.kt
+++ Project/Android/app/src/main/java/org/github/henryquan/battery/MainActivity.kt
@@ -6,7 +6,6 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import org.github.henryquan.battery.Core.BatteryUtil
 
@@ -14,8 +13,9 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var battery: BatteryUtil
     // Temp solution to get better estimate
-    val mah = arrayOf<Double>()
-    var curr = -1
+    val mah = mutableListOf<Double>()
+    var currLevel = -1
+    var currMah: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +23,6 @@ class MainActivity : AppCompatActivity() {
 
         // get an estimated value
         battery = BatteryUtil(this)
-        capacityLabel.text = battery.getEstimatedBatteryHealth()
 
         // add boardcast receiver to track battery level
         registerReceiver(this.broadCastReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED));
@@ -33,24 +32,29 @@ class MainActivity : AppCompatActivity() {
      * To update when battery level changed
      */
     val broadCastReceiver = object : BroadcastReceiver() {
-        override fun onReceive(contxt: Context?, intent: Intent?) {
-            val level = intent?.getIntExtra("level", -1)!!
+        override fun onReceive(context: Context, intent: Intent) {
+            val level = intent.getIntExtra("level", -1)
+            val capacity = battery.getCurrentCapacity()
 
-            if (curr == -1) {
-                curr = level
-            } else if (curr > level) {
+            // Start up also prevent possible incorrect valid
+            if (currLevel == -1) {
+                currLevel = level
+                currMah = capacity
+            }
+
+            // Update mah from the second update
+            if (level > currLevel) {
                 // update curr
-                curr = level
+                currLevel = level
+                val capacityDiff = capacity - currMah
                 // save current mah
-                mah.plus(battery.getCurrentCapacity())
+                mah.add(capacityDiff)
 
-                // With only one, the result will be zero...
-                if (mah.size > 1) {
-                    // Make first element zero because it might not be accurate
-                    mah[0] = 0.0
-                    // Get average capacity per 1%,
+                // Calculating your battery health
+                if (mah.size > 0) {
+                    // Get average capacity per 1%
                     val total = mah.reduce { acc, d -> acc + d }
-                    val estimate = total / (mah.size - 1) * 100.0
+                    val estimate = total / (mah.size ) * 100.0
                     val max = battery.getDesignedCapacity()
                     val percentage = String.format("%.2f", estimate / max * 100)
 
