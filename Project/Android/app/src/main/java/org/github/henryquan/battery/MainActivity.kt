322d3740fcf35825db11d8c0f02d5f6601d587d8
Yiheng <development.henryquan@gmail.com>
2018-12-15 21:08:49 +0800

    add log and fix some more logical issues

--- Project/Android/app/src/main/java/org/github/henryquan/battery/MainActivity.kt
+++ Project/Android/app/src/main/java/org/github/henryquan/battery/MainActivity.kt
@@ -6,8 +6,10 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import org.github.henryquan.battery.Core.BatteryUtil
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
 
@@ -42,23 +44,31 @@ class MainActivity : AppCompatActivity() {
                 currMah = capacity
             }
 
+            Log.d("Receiver", "curr ${level} | ${capacity}\nsaved - ${currLevel} | ${currMah}")
+
             // Update mah from the second update
             if (level > currLevel) {
+                // Take absolute so that it also works without charging
+                val capacityDiff = abs(capacity - currMah)
+
                 // update curr
                 currLevel = level
-                val capacityDiff = capacity - currMah
+                currMah = capacity
+
                 // save current mah
                 mah.add(capacityDiff)
 
                 // Calculating your battery health
-                if (mah.size > 0) {
+                if (mah.size > 1) {
+                    // Ignore the first one since it is corrupted
+                    mah[0] = 0.0
                     // Get average capacity per 1%
                     val total = mah.reduce { acc, d -> acc + d }
-                    val estimate = total / (mah.size ) * 100.0
+                    val estimate = total / (mah.size - 1)
                     val max = battery.getDesignedCapacity()
                     val percentage = String.format("%.2f", estimate / max * 100)
 
-                    capacityLabel.text = "${estimate} | ${percentage}% |${max} "
+                    capacityLabel.text = "${estimate} | ${percentage}% | ${max} "
                 }
             }
         }
