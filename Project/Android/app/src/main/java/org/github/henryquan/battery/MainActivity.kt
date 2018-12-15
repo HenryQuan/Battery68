b5818b639b85170776f9a1cd3f8aadf839c32b04
Yiheng <development.henryquan@gmail.com>
2018-12-15 15:11:26 +0800

    add a timer to update data every second
    The ideal solution is to use broadcast and get the number increased or decreased every 1 percent

--- Project/Android/app/src/main/java/org/github/henryquan/battery/MainActivity.kt
+++ Project/Android/app/src/main/java/org/github/henryquan/battery/MainActivity.kt
@@ -11,22 +11,44 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.Toast
 import java.lang.Class.*
 import android.os.Build
+import android.R.string.cancel
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     val savedMah = mutableListOf<Double>()
 
+    private var timer: Timer? = null
+
+    private val timerTask = object : TimerTask() {
+        override fun run() {
+            val batteryManager = this@MainActivity.getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            val chargeCounter = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CHARGE_COUNTER).toDouble() / 1000
+            val percentage = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY).toDouble()
+            val max = getDesignedCapacity()
+            val realP = String.format("%.2f", (chargeCounter / max * 100))
+            runOnUiThread {
+                capacityLabel.text = "${realP} / ${percentage} "
+                currentLabel.text = "${chargeCounter * 1000}";
+            }
+        }
+    }
+
+    fun start() {
+        if (timer != null) {
+            return
+        }
+        timer = Timer()
+        timer!!.scheduleAtFixedRate(timerTask, 0, 1000)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val curr = getCurrentCapacity(this)
-        val max = getDesignedCapacity()
-        val percentage = String.format("%.2f", (curr / max * 100))
-        capacityLabel.text = "${curr} / ${max} (${percentage}%)"
-        currentLabel.text = "${curr}";
-
-
+        start()
     }
     /**
      * Get device designed capacity and convert to a string
